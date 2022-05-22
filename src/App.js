1a92588437c1aaf824877db0534696e086eb3fee
Thomas Alex <thomas.a@tiflabs.in>
2022-05-22 20:12:24 +0530

    Refactored App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { useState, useEffect } from "react"
-import { saveAs } from 'file-saver'
 import './stylesheets/styles.css';
 
 import { MainSection } from "./components/main-section";
@@ -7,34 +5,47 @@ import { TaskComponent } from "./components/task-section";
 import { WeatherComponent } from "./components/weather-section";
 import { VocabularyComponent } from "./components/vocabulary-section";
 import { QuoteComponent } from "./components/quote-section";
+import { SettingsComponent } from "./components/settings-section";
+
+import { useUserPreferences } from "./contexts/user-pref-context";
 
 
 
 function App() {
 
-    const [bgImage, setBgImage] = useState("");
+    const { showSettingsPage, userSettingsPage, userPreferences } = useUserPreferences();
 
+    if (showSettingsPage === true) {
+        return (
+            userSettingsPage()
+        );
+    } else {
+        return (
+            <div className="App">
+                <section className="center-page">
+                    <MainSection />
+                    <TaskComponent />
+                </section>
+                
+                <section className="weather-section">
+                    {userPreferences.showWeather && <WeatherComponent />}
+                </section>
 
-    return (
-        <div className="App">
-            <section className="center-page">
-                <MainSection />
-                <TaskComponent />
-            </section>
+                <section className="quote-section">
+                    {userPreferences.showQuote && <QuoteComponent />}
+                </section>
 
-            <section className="weather-section">
-                {<WeatherComponent />}
-            </section>
+                <section className="vocab-section">
+                    {userPreferences.showVocab && <VocabularyComponent />}
+                </section>
 
-            <section className="quote-section">
-                <QuoteComponent />
-            </section>
-            
-            <section className="vocab-section">
-                <VocabularyComponent />
-            </section>
-        </div>
-    );
+                <section className="settings-section">
+                    <SettingsComponent />
+                </section>
+            </div>
+        );
+    }
+
 }
 
 export default App;
